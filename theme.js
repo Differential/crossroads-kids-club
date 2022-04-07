5491985ecdd7a0ed7302c46e5590963fd8a6f6ae
Michael Neeley <micneeley14@gmail.com>
2022-04-07 09:56:54 -0400

    remove share button

--- theme.js
+++ theme.js
@@ -7,7 +7,7 @@ import { makeIcon } from '@apollosproject/ui-kit';
 const THEME = {
   colors: { primary: '#C86600', secondary: '#C86600' },
   typography: {},
-  overrides: { 'ui-kit.ContentTitles': { onPressLike: 0 } },
+  overrides: { 'ui-kit.ContentTitles': { onPressLike: 0, onPressShare: 0 } },
 };
 
 const ICONS = {
